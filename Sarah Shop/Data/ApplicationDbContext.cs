80410a024a63d25449916db42d36b371210bf132
sarah-shtawi <sarah.sht03@gmail.com>
2026-01-28 03:49:58 +0200

    Connection With Data Base

---
+++ Sarah Shop/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Sarah_Shop.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer("server=. ; database=SarahShopMVC ; TrustServerCertificate= True; Trusted_Connection=True ");
+        }
+    }
+}
---
+++ Sarah Shop/Migrations/20260128014405_initail.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Sarah_Shop.Migrations
+{
+    /// <inheritdoc />
+    public partial class initail : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
