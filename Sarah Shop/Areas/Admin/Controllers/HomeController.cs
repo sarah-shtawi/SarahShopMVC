8cc75d067c1214bcc84dff56dc344fd55aefd9bb
sarah-shtawi <sarah.sht03@gmail.com>
2026-01-28 03:23:27 +0200

    Add project files.

---
+++ Sarah Shop/Areas/Admin/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Sarah_Shop.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Sarah Shop/Areas/User/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Sarah_Shop.Areas.User.Controllers
+{
+    [Area("User")]
+
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
